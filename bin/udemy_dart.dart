d204d5d62bd377ede898216869d7c09bfd9ef22c
Leonardo Gomes <leonardogomespadilha@gmail.com>
2022-01-27 21:20:46 -0300

    create a named constructor

--- bin/udemy_dart.dart
+++ bin/udemy_dart.dart
@@ -1,15 +1,24 @@
 class Microphone {
-  String name;
-  String color;
-  int model;
+  late String name;
+  late String color;
+  late int model;
 
   Microphone(this.name, this.color, this.model);
+
+  Microphone.initialize() {
+    name = 'Solocast';
+    color = 'Grey';
+  }
 }
 
 void main() {
   var mic = Microphone('Quadcast', 'Red', 1889);
+  var micDois = Microphone.initialize();
 
   print(mic.name);
   print(mic.color);
   print(mic.model);
+  print('                   ');
+  print(micDois.name);
+  print(micDois.color);
 }
